f1a954e6d747ccef5e8882714805d64c2142a690
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-09-29 01:38:11 -0600

    Parallax

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
+import Parallax from "@/components/Parallax";
 
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Parallax/>
   )
 }
