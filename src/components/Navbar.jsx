08b9b455be71ef708e879920635676cc36cb9091
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-10-02 02:18:06 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,104 +1,177 @@
-import Image from 'next/image'
-import React from 'react'
-import Logotrans from '../components/assets/Logos/logotrans.png'
-import {useRouter} from 'next/router'
-import Link from 'next/link'
-import { FaShoppingCart } from 'react-icons/fa' 
-import { FaUserLarge } from 'react-icons/fa6' 
-import {BiMenuAltRight} from 'react-icons/bi'
-import { motion, useTransform, useViewportScroll } from 'framer-motion';
-import { AppContext } from '@/context/StateContext'
+import Image from "next/image";
+import React from "react";
+import Logotrans from "../components/assets/Logos/logotrans.png";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUserLarge } from "react-icons/fa6";
+import {
+  BiLogoFacebookCircle,
+  BiLogoInstagram,
+  BiLogoTiktok,
+  BiLogoTwitch,
+  BiLogoTwitter,
+  BiMenuAltRight,
+} from "react-icons/bi";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
+import { AppContext } from "@/context/StateContext";
 
 export const navData = [
-    { name: 'home', path: '/', },
-    { name: 'About', path: '/About', },
-    { name: 'Shop', path: '/Shop', },
-    { name: 'Guides', path: '/Guides', },
-    { name: 'Advices', path: '/Advices', },
-]
+  { name: "home", path: "/" },
+  { name: "About", path: "/About" },
+  { name: "Shop", path: "/Shop" },
+  { name: "Guides", path: "/Guides" },
+  { name: "Advices", path: "/Advices" },
+];
 
 function Navbar() {
+  const router = useRouter();
+  const pathname = router.pathname;
+  // nav data
 
-    const router = useRouter()
-    const pathname = router.pathname
-// nav data
+  const { navButton, setNavButton } = AppContext();
 
-const {navButton, setNavButton} = AppContext()
-
-const cambioPagina = (item) => {
-    setNavButton(item.path)
+  const cambioPagina = (item) => {
+    setNavButton(item.path);
     setTimeout(() => {
-        router.push(item.path)
+      router.push(item.path);
     }, 4000);
-}
-
+  };
 
   return (
-    <div
-   
-    >
-    <motion.div 
-     initial={{ opacity: 0 }}
-     animate={{ opacity: 100 }}
-     transition={{duration:4}}
-    className='hidden absolute z-50 w-full h-[170px] xl:flex flex-row justify-between px-10'>
-        <div className='flex flex-row items-center'>
-            {/* Logo */}
-            <Image src={Logotrans} alt='logo' className='w-[150px] h-[150px] animate-pulse ' />
+    <div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 100 }}
+        transition={{ duration: 4 }}
+        className="hidden absolute z-50 w-full h-[170px] xl:flex flex-row justify-between px-10"
+      >
+        <div className="flex flex-row items-center">
+          {/* Logo */}
+          <Image
+            src={Logotrans}
+            alt="logo"
+            className="w-[150px] h-[150px] animate-pulse "
+          />
         </div>
-        <div className='flex flex-row'>
-            {/* Opciones menu */}
-            <ul className='w-full flex flex-row items-center gap-x-10 uppercase
+        <div className="flex flex-row">
+          {/* Opciones menu */}
+          <ul
+            className="w-full flex flex-row items-center gap-x-10 uppercase
             text-4xl 
-            '>
-                {
-                    navData.map((item, index) =>(
-                        <div key={index} 
-                        href={item.path}
-                        onClick={(()=>cambioPagina(item))}
-                        >
-                            
-                        <li className={item.path == pathname?"w-full relative text-white":'w-[50%] text-black h-full flex flex-col relative cursor-pointer hover:text-white transition-all duration-300'}>
-                            <span className='text-[35px] font-MelbergRegular tracking-[2px]'>{item.name}</span>
-                            </li>
-                        </div>
-                    ))
-                }
-            </ul>
+            "
+          >
+            {navData.map((item, index) => (
+              <div
+                key={index}
+                href={item.path}
+                onClick={() => cambioPagina(item)}
+              >
+                <li
+                  className={
+                    item.path == pathname
+                      ? "w-full relative text-white"
+                      : "w-[50%] text-black h-full flex flex-col relative cursor-pointer hover:text-white transition-all duration-300"
+                  }
+                >
+                  <span className="text-[35px] font-MelbergRegular tracking-[2px]">
+                    {item.name}
+                  </span>
+                </li>
+              </div>
+            ))}
+          </ul>
         </div>
-        <div className='flex flex-row items-center justify-center gap-x-8'>
-            {/* Carrito */}
-            <div>
-                <FaShoppingCart className='text-3xl hover:text-white transition-all duration-300 cursor-pointer' />
-            </div>
-            {/* User */}
-            <div>
-                <FaUserLarge className='text-3xl hover:text-white transition-all duration-300 cursor-pointer'/>
-            </div>
+        <div className="flex flex-row items-center justify-center gap-x-8">
+          {/* Carrito */}
+          <div>
+            <FaShoppingCart className="text-3xl hover:text-white transition-all duration-300 cursor-pointer" />
+          </div>
+          {/* User */}
+          <div>
+            <FaUserLarge className="text-3xl hover:text-white transition-all duration-300 cursor-pointer" />
+          </div>
         </div>
-    </motion.div>
-    {/* Movil */}
-    <motion.div 
-    initial={{ translateY: -100 }}
-    animate={{ translateY: 0 }}
-    transition={{duration:4}}
-    className='w-full h-[100px] absolute top-0 z-50 flex flex-row justify-between items-center xl:hidden'>
-            {/* Logo */}
-            <Image src={Logotrans} alt='logo' className='w-[100px] h-[100px] animate-pulse ' />
-            {/* Hamburguer */}
-            <div> 
-                <BiMenuAltRight className='mr-4 text-[60px] text-[#fed0b1] animate-pulse
+      </motion.div>
+      {/* Movil */}
+      <motion.div
+        initial={{ translateY: -100 }}
+        animate={{ translateY: 0 }}
+        transition={{ duration: 4 }}
+        className="w-full h-[100px] absolute top-0 z-50 flex flex-row justify-between items-center xl:hidden"
+      >
+        {/* Logo */}
+        <Image
+          src={Logotrans}
+          alt="logo"
+          className="w-[100px] h-[100px] animate-pulse "
+        />
+        {/* Hamburguer */}
+        <div>
+          <BiMenuAltRight
+            className="mr-4 text-[60px] text-[#fed0b1] animate-pulse
                 
-                cursor-pointer'/>
-            </div>
-        
-    </motion.div>
-    {/* Menu */}
-    <div className='w-full h'>
-
+                cursor-pointer"
+          />
+        </div>
+      </motion.div>
+      {/* Menu */}
+      <div
+        className="w-full h-screen flex flex-col 
+      items-center bg-red-500 gap-y-4"
+      >
+        <div>
+          <Image src={Logotrans} alt="logo" className="w-[150px]" />
+        </div>
+        <ul
+          className="text-white text-center uppercase
+        flex flex-col w-full 
+        gap-y-6
+        text-2xl
+        "
+        >
+          {navData.map((item, index) => (
+            <li key={item}>
+              <span>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+        <div
+          className="w-full  flex flex-row justify-around
+        items-center 
+        "
+        >
+          <div className="flex flex-col justify-center items-center bg-gray-500 w-[70px] h-[70px] rounded-[25px] ">
+            <BiLogoFacebookCircle className="text-4xl " />
+          </div>
+          <div className="flex flex-col justify-center items-center bg-gray-500 w-[70px] h-[70px] rounded-[25px]">
+            <BiLogoInstagram className="text-4xl " />
+          </div>
+          <div className="flex flex-col justify-center items-center bg-gray-500 w-[70px] h-[70px] rounded-[25px]">
+            <BiLogoTiktok className="text-4xl " />
+          </div>
+          <div className="flex flex-col justify-center items-center bg-gray-500 w-[70px] h-[70px] rounded-[25px]">
+            <BiLogoTwitter className="text-4xl " />
+          </div>
+          <div className="flex flex-col justify-center items-center bg-gray-500 w-[70px] h-[70px] rounded-[25px]">
+            <BiLogoTwitch className="text-4xl " />
+          </div>
+        </div>
+        <div className="flex flex-col items-center w-full text-white gap-y-4">
+          <div className="w-full">
+            <button className=" py-4 bg-transparent border-white border w-full rounded-[25px]  ">
+              Log In
+            </button>
+          </div>
+          <div className="w-full">
+            <button className=" py-4 bg-transparent border-white border w-full rounded-[25px]  ">
+              Sign In
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
